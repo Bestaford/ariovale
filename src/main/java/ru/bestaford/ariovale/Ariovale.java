5b6fe27e29eac9c2e4d2329aab96a9bc271c61d4
Bestaford <butenko1demitr@gmail.com>
2023-03-16 00:41:27 +0300

    Make class final

--- src/main/java/ru/bestaford/ariovale/Ariovale.java
+++ src/main/java/ru/bestaford/ariovale/Ariovale.java
@@ -4,7 +4,7 @@ import cn.nukkit.plugin.PluginBase;
 import ru.bestaford.ariovale.annotation.EntryPoint;
 
 @EntryPoint
-public class Ariovale extends PluginBase {
+public final class Ariovale extends PluginBase {
 
     @Override
     public void onEnable() {
