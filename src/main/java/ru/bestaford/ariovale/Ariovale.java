b5d85cb2351ae35436daece82b2031d9609de2a9
bestaford <butenko1demitr@gmail.com>
2023-03-15 14:25:14 +0300

    Initial commit

---
+++ src/main/java/ru/bestaford/ariovale/Ariovale.java
@@ -0,0 +1,11 @@
+package ru.bestaford.ariovale;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class Ariovale extends PluginBase {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello world!");
+    }
+}
