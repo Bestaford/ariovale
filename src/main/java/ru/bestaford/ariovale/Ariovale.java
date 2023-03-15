bd11571fcabb1f715db9dde50d8229a0140dc098
Bestaford <butenko1demitr@gmail.com>
2023-03-16 00:34:54 +0300

    Entry point annotation

--- src/main/java/ru/bestaford/ariovale/Ariovale.java
+++ src/main/java/ru/bestaford/ariovale/Ariovale.java
@@ -1,7 +1,9 @@
 package ru.bestaford.ariovale;
 
 import cn.nukkit.plugin.PluginBase;
+import ru.bestaford.ariovale.annotation.EntryPoint;
 
+@EntryPoint
 public class Ariovale extends PluginBase {
 
     @Override
