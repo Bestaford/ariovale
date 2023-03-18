15a3b870ee80d1c5f9d1a6001d13694c95d005b0
Bestaford <butenko1demitr@gmail.com>
2023-03-18 16:20:42 +0300

    Event listeners

--- src/main/java/ru/bestaford/ariovale/Core.java
+++ src/main/java/ru/bestaford/ariovale/Core.java
@@ -1,10 +1,14 @@
 package ru.bestaford.ariovale;
 
+import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import ru.bestaford.ariovale.annotation.EntryPoint;
+import ru.bestaford.ariovale.listener.AuthorizationListener;
 import ru.bestaford.ariovale.module.CoreModule;
+
+import java.util.ArrayList;
 
 @EntryPoint
 public final class Core extends PluginBase {
@@ -17,6 +21,17 @@ public final class Core extends PluginBase {
 
     @Override
     public void onEnable() {
-        getLogger().info(injector.toString());
+        registerListeners();
+    }
+
+    private void registerListeners() {
+        var pluginManager = getServer().getPluginManager();
+        var listeners = new ArrayList<Class<? extends Listener>>();
+
+        listeners.add(AuthorizationListener.class);
+
+        for (var listener : listeners) {
+            pluginManager.registerEvents(injector.getInstance(listener), this);
+        }
     }
 }
