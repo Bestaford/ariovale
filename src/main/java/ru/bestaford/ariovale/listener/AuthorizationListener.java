c784c0f78c9c9d43806371f1a8b8482a507464ba
Bestaford <butenko1demitr@gmail.com>
2023-03-18 16:29:59 +0300

    Event listeners

--- src/main/java/ru/bestaford/ariovale/listener/AuthorizationListener.java
+++ src/main/java/ru/bestaford/ariovale/listener/AuthorizationListener.java
@@ -1,10 +1,17 @@
 package ru.bestaford.ariovale.listener;
 
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
 
 import javax.inject.Singleton;
 
 @Singleton
-public class AuthorizationListener implements Listener {
+public final class AuthorizationListener implements Listener {
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Hello!");
+    }
 }
