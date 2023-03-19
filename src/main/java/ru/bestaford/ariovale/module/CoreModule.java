321c669c270ea744db7455e8ba663b3e0ecf9249
Bestaford <butenko1demitr@gmail.com>
2023-03-19 10:40:08 +0300

    Module

--- src/main/java/ru/bestaford/ariovale/module/CoreModule.java
+++ src/main/java/ru/bestaford/ariovale/module/CoreModule.java
@@ -1,7 +1,5 @@
 package ru.bestaford.ariovale.module;
 
-import cn.nukkit.Server;
-import cn.nukkit.utils.Logger;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import ru.bestaford.ariovale.Core;
@@ -17,15 +15,5 @@ public final class CoreModule extends AbstractModule {
     @Provides
     public Core provideCore() {
         return core;
-    }
-
-    @Provides
-    public Server provideServer() {
-        return core.getServer();
-    }
-
-    @Provides
-    public Logger provideLogger() {
-        return core.getLogger();
     }
 }
