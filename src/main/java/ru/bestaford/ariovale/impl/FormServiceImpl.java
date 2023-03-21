4e7a24f3db5dabed66dfca4aa31ea5229887d6d2
bestaford <butenko1demitr@gmail.com>
2023-03-21 13:27:08 +0300

    Forms skeleton

--- src/main/java/ru/bestaford/ariovale/impl/FormServiceImpl.java
+++ src/main/java/ru/bestaford/ariovale/impl/FormServiceImpl.java
@@ -1,10 +1,24 @@
 package ru.bestaford.ariovale.impl;
 
+import com.google.inject.Injector;
+import ru.bestaford.ariovale.form.base.Form;
 import ru.bestaford.ariovale.service.FormService;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public final class FormServiceImpl implements FormService {
 
+    private final Injector injector;
+
+    @Inject
+    public FormServiceImpl(Injector injector) {
+        this.injector = injector;
+    }
+
+    @Override
+    public <T extends Form> T createForm(Class<T> formClass) {
+        return injector.getInstance(formClass);
+    }
 }
--- src/main/java/ru/bestaford/ariovale/service/FormService.java
+++ src/main/java/ru/bestaford/ariovale/service/FormService.java
@@ -1,5 +1,9 @@
 package ru.bestaford.ariovale.service;
 
+import ru.bestaford.ariovale.form.base.Form;
+
 public interface FormService {
+
+    <T extends Form> T createForm(Class<T> formClass);
 
 }
