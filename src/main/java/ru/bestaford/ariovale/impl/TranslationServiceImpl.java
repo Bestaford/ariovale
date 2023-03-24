1569510d6d3f3c00472b910a20870ff1453d1eb5
bestaford <butenko1demitr@gmail.com>
2023-03-24 11:31:03 +0300

    TranslationService

--- src/main/java/ru/bestaford/ariovale/impl/TranslationServiceImpl.java
+++ src/main/java/ru/bestaford/ariovale/impl/TranslationServiceImpl.java
@@ -1,9 +1,25 @@
 package ru.bestaford.ariovale.impl;
 
+import cn.nukkit.Player;
 import com.google.inject.Singleton;
 import ru.bestaford.ariovale.service.TranslationService;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @Singleton
 public final class TranslationServiceImpl implements TranslationService {
 
+    private static final String BUNDLE_BASE_NAME = "strings";
+
+    @Override
+    public String getString(String key) {
+        return ResourceBundle.getBundle(BUNDLE_BASE_NAME).getString(key);
+    }
+
+    @Override
+    public String getString(String key, Player player) {
+        String[] languageCodeParts = player.getLoginChainData().getLanguageCode().split("_");
+        return ResourceBundle.getBundle(BUNDLE_BASE_NAME, new Locale(languageCodeParts[0], languageCodeParts[1])).getString(key);
+    }
 }
--- src/main/java/ru/bestaford/ariovale/service/TranslationService.java
+++ src/main/java/ru/bestaford/ariovale/service/TranslationService.java
@@ -1,5 +1,11 @@
 package ru.bestaford.ariovale.service;
 
+import cn.nukkit.Player;
+
 public interface TranslationService {
+
+    String getString(String key);
+
+    String getString(String key, Player player);
 
 }
