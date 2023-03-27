3c7c5ee7786ed5f604d648c84aa0b012ca060754
Bestaford <butenko1demitr@gmail.com>
2023-03-27 16:59:20 +0300

    Forms

--- src/main/java/ru/bestaford/ariovale/listener/impl/FormListenerImpl.java
+++ src/main/java/ru/bestaford/ariovale/listener/impl/FormListenerImpl.java
@@ -4,15 +4,24 @@ import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.player.PlayerFormRespondedEvent;
 import ru.bestaford.ariovale.listener.FormListener;
+import ru.bestaford.ariovale.service.FormService;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public final class FormListenerImpl implements FormListener {
 
+    private final FormService formService;
+
+    @Inject
+    public FormListenerImpl(FormService formService) {
+        this.formService = formService;
+    }
+
     @Override
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerFormResponded(PlayerFormRespondedEvent event) {
-
+        formService.handleResponse(event.getFormID(), event.getPlayer(), event.wasClosed(), event.getResponse());
     }
 }
