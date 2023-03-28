a6ff4e89681897c01e330367f91ba5a0f8b42d97
Bestaford <butenko1demitr@gmail.com>
2023-03-28 21:56:07 +0300

    Refactor

--- src/main/java/ru/bestaford/ariovale/module/CoreModule.java
+++ src/main/java/ru/bestaford/ariovale/module/CoreModule.java
@@ -3,12 +3,12 @@ package ru.bestaford.ariovale.module;
 import cn.nukkit.utils.Logger;
 import com.google.inject.AbstractModule;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import ru.bestaford.ariovale.Core;
 import ru.bestaford.ariovale.listener.AuthorizationListener;
 import ru.bestaford.ariovale.listener.FormListener;
 import ru.bestaford.ariovale.listener.impl.AuthorizationListenerImpl;
 import ru.bestaford.ariovale.listener.impl.FormListenerImpl;
-import ru.bestaford.ariovale.module.provider.SessionFactoryProvider;
 import ru.bestaford.ariovale.service.AuthorizationService;
 import ru.bestaford.ariovale.service.FormService;
 import ru.bestaford.ariovale.service.TranslationService;
@@ -30,6 +30,9 @@ public final class CoreModule extends AbstractModule {
         bind(Core.class).toInstance(core);
         bind(Logger.class).toInstance(core.getLogger());
 
+        //Hibernate
+        bind(SessionFactory.class).toInstance(new Configuration().configure().buildSessionFactory());
+
         //Services
         bind(AuthorizationService.class).to(AuthorizationServiceImpl.class).asEagerSingleton();
         bind(FormService.class).to(FormServiceImpl.class).asEagerSingleton();
@@ -38,8 +41,5 @@ public final class CoreModule extends AbstractModule {
         //Listeners
         bind(AuthorizationListener.class).to(AuthorizationListenerImpl.class).asEagerSingleton();
         bind(FormListener.class).to(FormListenerImpl.class).asEagerSingleton();
-
-        //Hibernate
-        bind(SessionFactory.class).toProvider(SessionFactoryProvider.class).asEagerSingleton();
     }
 }
