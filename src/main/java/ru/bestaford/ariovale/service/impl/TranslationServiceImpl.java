9baaaedf6ca7a6bb8fc6d23066037444f9efff05
Bestaford <butenko1demitr@gmail.com>
2023-04-01 19:18:27 +0300

    Text variables

--- src/main/java/ru/bestaford/ariovale/service/impl/TranslationServiceImpl.java
+++ src/main/java/ru/bestaford/ariovale/service/impl/TranslationServiceImpl.java
@@ -6,6 +6,8 @@ import ru.bestaford.ariovale.service.TranslationService;
 import javax.inject.Singleton;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Singleton
 public final class TranslationServiceImpl implements TranslationService {
@@ -16,6 +18,14 @@ public final class TranslationServiceImpl implements TranslationService {
     public String getString(String key, Player player) {
         String[] languageCodeParts = player.getLoginChainData().getLanguageCode().split("_");
         Locale locale = new Locale(languageCodeParts[0], languageCodeParts[1]);
-        return ResourceBundle.getBundle(BUNDLE_BASE_NAME, locale).getString(key).trim();
+        String source = ResourceBundle.getBundle(BUNDLE_BASE_NAME, locale).getString(key).trim();
+        StringBuilder result = new StringBuilder();
+        Pattern pattern = Pattern.compile("%\\{(.*?)}");
+        Matcher matcher = pattern.matcher(source);
+        while (matcher.find()) {
+            matcher.appendReplacement(result, getString(matcher.group(1), player));
+        }
+        matcher.appendTail(result);
+        return result.toString();
     }
 }
