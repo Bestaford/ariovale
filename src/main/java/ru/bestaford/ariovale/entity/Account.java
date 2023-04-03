53c52916362bc0ee91911884d72a1171f1cf7d47
bestaford <butenko1demitr@gmail.com>
2023-04-03 12:34:42 +0300

    JPA

--- src/main/java/ru/bestaford/ariovale/entity/Account.java
+++ src/main/java/ru/bestaford/ariovale/entity/Account.java
@@ -1,5 +1,6 @@
 package ru.bestaford.ariovale.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -9,6 +10,7 @@ import jakarta.persistence.Table;
 public class Account {
 
     @Id
+    @Column(nullable = false)
     private String name;
 
     public Account() {
