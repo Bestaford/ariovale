37710c8d23b9ca82b0d112e1c92878069f83d634
bestaford <butenko1demitr@gmail.com>
2023-04-03 14:51:50 +0300

    RegistrationForm

--- src/main/java/ru/bestaford/ariovale/form/RegistrationForm.java
+++ src/main/java/ru/bestaford/ariovale/form/RegistrationForm.java
@@ -1,6 +1,7 @@
 package ru.bestaford.ariovale.form;
 
 import cn.nukkit.Player;
+import cn.nukkit.form.element.ElementInput;
 import cn.nukkit.form.element.ElementLabel;
 import cn.nukkit.form.response.FormResponseCustom;
 import ru.bestaford.ariovale.service.TranslationService;
@@ -28,6 +29,10 @@ public final class RegistrationForm extends CustomForm {
     public void build(Player player) {
         setTitle(translationService.getString("registration.form.title", player));
         addElement(new ElementLabel(translationService.getString("registration.form.label", player)));
+        addElement(new ElementInput(
+                translationService.getString("registration.form.input.text", player),
+                translationService.getString("registration.form.input.placeholder", player)
+        ));
     }
 
     @Override
