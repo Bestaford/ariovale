80b52826d60cd31122c2f913d322f2d9267c8592
Bestaford <butenko1demitr@gmail.com>
2023-04-06 15:07:39 +0300

    Enumerated field

--- src/main/java/ru/bestaford/ariovale/entity/Account.java
+++ src/main/java/ru/bestaford/ariovale/entity/Account.java
@@ -1,9 +1,6 @@
 package ru.bestaford.ariovale.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import ru.bestaford.ariovale.util.Sex;
 
 @Entity
@@ -21,6 +18,7 @@ public class Account {
     private String password;
 
     @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
     private Sex sex;
 
     @Column(nullable = false)
