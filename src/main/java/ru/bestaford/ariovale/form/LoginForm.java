b51aba0d9b6c1b8cf08c8c369fc0b0589b89e9bd
Bestaford <butenko1demitr@gmail.com>
2023-04-16 15:13:13 +0300

    LoginForm

--- src/main/java/ru/bestaford/ariovale/form/LoginForm.java
+++ src/main/java/ru/bestaford/ariovale/form/LoginForm.java
@@ -8,19 +8,23 @@ import ru.bestaford.ariovale.entity.Account;
 import ru.bestaford.ariovale.form.base.CustomForm;
 import ru.bestaford.ariovale.form.base.Form;
 import ru.bestaford.ariovale.form.base.Required;
+import ru.bestaford.ariovale.service.FormService;
 import ru.bestaford.ariovale.service.TranslationService;
 
 import javax.inject.Inject;
+import java.util.Objects;
 
 @Required
 public final class LoginForm extends CustomForm {
 
+    private final transient FormService formService;
     private transient Account account;
     private transient String error;
 
     @Inject
-    public LoginForm(TranslationService translationService) {
+    public LoginForm(TranslationService translationService, FormService formService) {
         super(translationService);
+        this.formService = formService;
     }
 
     public void setAccount(Account account) {
@@ -34,7 +38,7 @@ public final class LoginForm extends CustomForm {
     @Override
     public void build(Player player) {
         setTitle(translationService.getString("login.form.title", player));
-        addElement(new ElementLabel(translationService.getString("login.form.label", player) + ": " + account.getName()));
+        addElement(new ElementLabel(translationService.getString(Objects.requireNonNullElse(error, "login.form.label"), player)));
         addElement(new ElementInput(
                 translationService.getString("login.form.input.text", player),
                 translationService.getString("login.form.input.placeholder", player)
@@ -52,6 +56,14 @@ public final class LoginForm extends CustomForm {
 
     @Override
     public void handle(Player player, boolean wasClosed, FormResponseCustom response) {
-
+        String password = response.getInputResponse(1);
+        account.setPassword(password);
+        error = null;
+        if (password.isBlank()) {
+            account.setPassword(null);
+            error = "login.form.input.error.empty";
+            formService.sendCopy(this, player);
+            return;
+        }
     }
 }
