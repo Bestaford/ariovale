e3ef4387d3a8cfcb97a0c39e03f322e59f6c8dd9
bestaford <butenko1demitr@gmail.com>
2023-04-17 10:35:59 +0300

    Forms refactor

--- src/main/java/ru/bestaford/ariovale/listener/FormListener.java
+++ src/main/java/ru/bestaford/ariovale/listener/FormListener.java
@@ -1,10 +1,26 @@
 package ru.bestaford.ariovale.listener;
 
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerFormRespondedEvent;
+import ru.bestaford.ariovale.service.FormService;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public final class FormListener implements Listener {
 
+    private final FormService formService;
+
+    @Inject
+    public FormListener(FormService formService) {
+        this.formService = formService;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerFormResponded(PlayerFormRespondedEvent event) {
+        formService.handleResponse(event.getWindow(), event.getPlayer(), event.wasClosed(), event.getResponse());
+    }
 }
--- src/main/java/ru/bestaford/ariovale/service/FormService.java
+++ src/main/java/ru/bestaford/ariovale/service/FormService.java
@@ -1,8 +1,52 @@
 package ru.bestaford.ariovale.service;
 
+import cn.nukkit.Player;
+import cn.nukkit.form.response.FormResponse;
+import cn.nukkit.form.response.FormResponseCustom;
+import cn.nukkit.form.response.FormResponseModal;
+import cn.nukkit.form.response.FormResponseSimple;
+import cn.nukkit.form.window.FormWindow;
+import com.google.inject.Injector;
+import ru.bestaford.ariovale.form.base.CustomForm;
+import ru.bestaford.ariovale.form.base.Form;
+import ru.bestaford.ariovale.form.base.ModalForm;
+import ru.bestaford.ariovale.form.base.SimpleForm;
+
+import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Singleton
 public final class FormService {
 
+    private final Injector injector;
+    private final Map<FormWindow, Form> windowMap;
+
+    @Inject
+    public FormService(Injector injector) {
+        this.injector = injector;
+        windowMap = new ConcurrentHashMap<>();
+    }
+
+    public void sendForm(Form form, Player player) {
+        injector.injectMembers(form);
+        FormWindow window = form.getWindow(player);
+        windowMap.put(window, form);
+        player.showFormWindow(window);
+    }
+
+    public void handleResponse(FormWindow window, Player player, boolean wasClosed, FormResponse response) {
+        if (windowMap.containsKey(window)) {
+            Form form = windowMap.get(window);
+            if (form instanceof SimpleForm) {
+                ((SimpleForm) form).handle(player, wasClosed, (FormResponseSimple) response);
+            } else if (form instanceof ModalForm) {
+                ((ModalForm) form).handle(player, wasClosed, (FormResponseModal) response);
+            } else if (form instanceof CustomForm) {
+                ((CustomForm) form).handle(player, wasClosed, (FormResponseCustom) response);
+            }
+            windowMap.remove(window);
+        }
+    }
 }
