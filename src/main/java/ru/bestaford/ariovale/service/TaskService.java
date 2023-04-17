1db8428b535cb38b3680b9c8bdbf338d1f74dd73
bestaford <butenko1demitr@gmail.com>
2023-04-17 10:59:12 +0300

    TaskService

--- src/main/java/ru/bestaford/ariovale/service/TaskService.java
+++ src/main/java/ru/bestaford/ariovale/service/TaskService.java
@@ -1,8 +1,29 @@
 package ru.bestaford.ariovale.service;
 
+import cn.nukkit.scheduler.AsyncTask;
+import cn.nukkit.scheduler.ServerScheduler;
+import com.google.inject.Injector;
+import ru.bestaford.ariovale.Core;
+
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public final class TaskService {
 
+    private final Injector injector;
+    private final Core core;
+    private final ServerScheduler scheduler;
+
+    @Inject
+    public TaskService(Injector injector, Core core, ServerScheduler scheduler) {
+        this.injector = injector;
+        this.core = core;
+        this.scheduler = scheduler;
+    }
+
+    public void scheduleAsyncTask(AsyncTask task) {
+        injector.injectMembers(task);
+        scheduler.scheduleAsyncTask(core, task);
+    }
 }
