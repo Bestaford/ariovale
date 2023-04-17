fc069c396089a5c9b60aec8652066a5a891ca4a5
bestaford <butenko1demitr@gmail.com>
2023-04-17 10:43:49 +0300

    AuthenticationService

--- src/main/java/ru/bestaford/ariovale/listener/AuthenticationListener.java
+++ src/main/java/ru/bestaford/ariovale/listener/AuthenticationListener.java
@@ -1,10 +1,32 @@
 package ru.bestaford.ariovale.listener;
 
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerLocallyInitializedEvent;
+import ru.bestaford.ariovale.service.AuthenticationService;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public final class AuthenticationListener implements Listener {
 
+    private final AuthenticationService authenticationService;
+
+    @Inject
+    public AuthenticationListener(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        authenticationService.initialize(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerLocallyInitialized(PlayerLocallyInitializedEvent event) {
+        authenticationService.process(event.getPlayer());
+    }
 }
--- src/main/java/ru/bestaford/ariovale/service/AuthenticationService.java
+++ src/main/java/ru/bestaford/ariovale/service/AuthenticationService.java
@@ -1,8 +1,27 @@
 package ru.bestaford.ariovale.service;
 
+import cn.nukkit.Player;
+import cn.nukkit.math.Vector3;
+
 import javax.inject.Singleton;
 
 @Singleton
 public final class AuthenticationService {
 
+    public void initialize(Player player) {
+        player.setGamemode(Player.SURVIVAL);
+        player.setAllowModifyWorld(false);
+        player.setAllowInteract(false);
+        player.setCheckMovement(false);
+        player.setOp(false);
+        int x = player.getFloorX();
+        int z = player.getFloorZ();
+        int y = player.getLevel().getHighestBlockAt(x, z);
+        player.teleport(new Vector3(x, y, z));
+        player.pitch = 0;
+    }
+
+    public void process(Player player) {
+        player.sendMessage("process");
+    }
 }
