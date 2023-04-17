0b767803e173136992bd65c0f3e3a18d146f0df4
bestaford <butenko1demitr@gmail.com>
2023-04-17 12:53:57 +0300

    AuthenticationForm

--- src/main/java/ru/bestaford/ariovale/form/AuthenticationForm.java
+++ src/main/java/ru/bestaford/ariovale/form/AuthenticationForm.java
@@ -47,5 +47,14 @@ public final class AuthenticationForm extends CustomForm {
             formService.sendForm(this, player);
             return;
         }
+        StringBuilder finalName = new StringBuilder();
+        String[] nameParts = StringUtils.split(name);
+        finalName
+                .append(nameParts[0].substring(0, 1).toUpperCase())
+                .append(nameParts[0].substring(1).toLowerCase())
+                .append(" ")
+                .append(nameParts[1].substring(0, 1).toUpperCase())
+                .append(nameParts[1].substring(1).toLowerCase());
+        name = finalName.toString().trim();
     }
 }
