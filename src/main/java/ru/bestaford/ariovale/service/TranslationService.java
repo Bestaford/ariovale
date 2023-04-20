0691af6f84d1a2e08f20907c49662daf6161adbc
bestaford <butenko1demitr@gmail.com>
2023-04-20 12:26:28 +0300

    Refactor

--- src/main/java/ru/bestaford/ariovale/service/TranslationService.java
+++ src/main/java/ru/bestaford/ariovale/service/TranslationService.java
@@ -12,12 +12,8 @@ public final class TranslationService {
     private static final String BUNDLE_BASE_NAME = "strings";
 
     public String getString(Player player, String key, Object... args) {
-        return getString(player, key).formatted(args);
-    }
-
-    public String getString(Player player, String key) {
         String[] languageCodeParts = player.getLoginChainData().getLanguageCode().split("_");
         Locale locale = new Locale(languageCodeParts[0], languageCodeParts[1]);
-        return ResourceBundle.getBundle(BUNDLE_BASE_NAME, locale).getString(key).trim();
+        return ResourceBundle.getBundle(BUNDLE_BASE_NAME, locale).getString(key).trim().formatted(args);
     }
 }
