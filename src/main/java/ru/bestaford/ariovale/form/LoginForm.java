fcde78ca5527db7eccdf60f9a157fa9ae26af1c2
bestaford <butenko1demitr@gmail.com>
2023-04-21 10:50:29 +0300

    LoginForm

--- src/main/java/ru/bestaford/ariovale/form/LoginForm.java
+++ src/main/java/ru/bestaford/ariovale/form/LoginForm.java
@@ -1,6 +1,8 @@
 package ru.bestaford.ariovale.form;
 
 import cn.nukkit.Player;
+import cn.nukkit.form.element.ElementInput;
+import cn.nukkit.form.element.ElementLabel;
 import cn.nukkit.form.response.FormResponseCustom;
 import ru.bestaford.ariovale.entity.Account;
 import ru.bestaford.ariovale.form.base.CustomForm;
@@ -8,6 +10,7 @@ import ru.bestaford.ariovale.form.base.Required;
 import ru.bestaford.ariovale.service.TranslationService;
 
 import javax.inject.Inject;
+import java.util.Objects;
 
 @Required
 public class LoginForm extends CustomForm {
@@ -26,6 +29,12 @@ public class LoginForm extends CustomForm {
     @Override
     protected void build(Player player) {
         window.setTitle(FORMAT_BOLD + translationService.getString(player, "login.form.title"));
+        window.addElement(new ElementLabel(Objects.requireNonNullElseGet(error, () -> translationService.getString(player, "login.form.label") + COLON + SPACE + THEME_PRIMARY + account.getName())));
+        window.addElement(new ElementInput(
+                translationService.getString(player, "registration.form.input.text"),
+                translationService.getString(player, "registration.form.input.placeholder"),
+                Objects.requireNonNullElse(password, EMPTY)
+        ));
     }
 
     @Override
