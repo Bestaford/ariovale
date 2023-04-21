dc19e3c7096c3650f3dd2815de9888b887b88a3c
bestaford <butenko1demitr@gmail.com>
2023-04-21 13:35:47 +0300

    Login

--- src/main/java/ru/bestaford/ariovale/task/LoginTask.java
+++ src/main/java/ru/bestaford/ariovale/task/LoginTask.java
@@ -1,10 +1,12 @@
 package ru.bestaford.ariovale.task;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import cn.nukkit.Player;
 import cn.nukkit.Server;
 import cn.nukkit.scheduler.AsyncTask;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import ru.bestaford.ariovale.entity.Account;
 import ru.bestaford.ariovale.service.GameService;
 
 import javax.inject.Inject;
@@ -15,6 +17,7 @@ public final class LoginTask extends AsyncTask {
     private final String name;
     private final String password;
 
+    private boolean verified;
     private boolean success;
 
     @Inject private SessionFactory sessionFactory;
@@ -29,6 +32,8 @@ public final class LoginTask extends AsyncTask {
     @Override
     public void onRun() {
         try (Session session = sessionFactory.openSession()) {
+            Account account = session.get(Account.class, name);
+            verified = BCrypt.verifyer().verify(password.toCharArray(), account.getPassword().toCharArray()).verified;
             success = true;
         }
     }
@@ -36,7 +41,7 @@ public final class LoginTask extends AsyncTask {
     @Override
     public void onCompletion(Server server) {
         if (player.isOnline() && success) {
-            player.sendMessage("login");
+            player.sendMessage(String.valueOf(verified));
         } else {
             gameService.closeWithError(player);
         }
