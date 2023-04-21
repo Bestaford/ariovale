1abee069604f9809b98af92efed00e073f1068af
bestaford <butenko1demitr@gmail.com>
2023-04-21 12:15:51 +0300

    Refactor

--- src/main/java/ru/bestaford/ariovale/form/AuthenticationForm.java
+++ src/main/java/ru/bestaford/ariovale/form/AuthenticationForm.java
@@ -4,7 +4,7 @@ import cn.nukkit.Player;
 import cn.nukkit.form.element.ElementInput;
 import cn.nukkit.form.element.ElementLabel;
 import cn.nukkit.form.response.FormResponseCustom;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.WordUtils;
 import ru.bestaford.ariovale.entity.Account;
 import ru.bestaford.ariovale.form.base.CustomForm;
 import ru.bestaford.ariovale.form.base.Required;
@@ -51,15 +51,7 @@ public final class AuthenticationForm extends CustomForm {
             formService.sendForm(this, player);
             return;
         }
-        StringBuilder finalName = new StringBuilder();
-        String[] nameParts = StringUtils.split(name);
-        finalName
-                .append(nameParts[0].substring(0, 1).toUpperCase())
-                .append(nameParts[0].substring(1).toLowerCase())
-                .append(SPACE)
-                .append(nameParts[1].substring(0, 1).toUpperCase())
-                .append(nameParts[1].substring(1).toLowerCase());
-        name = finalName.toString().trim();
+        name = WordUtils.capitalizeFully(name);
         authenticationService.authenticate(player, name);
     }
 }
