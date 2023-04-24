8b40061d7ec45a061f6b68551185714f4255195a
bestaford <butenko1demitr@gmail.com>
2023-04-24 10:03:46 +0300

    PNX

--- src/main/java/ru/bestaford/ariovale/Core.java
+++ src/main/java/ru/bestaford/ariovale/Core.java
@@ -4,17 +4,18 @@ import cn.nukkit.Server;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.plugin.PluginManager;
 import cn.nukkit.scheduler.ServerScheduler;
-import cn.nukkit.utils.MainLogger;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
+import lombok.extern.log4j.Log4j2;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.bestaford.ariovale.listener.AuthenticationListener;
 import ru.bestaford.ariovale.listener.FormListener;
 import ru.bestaford.ariovale.service.*;
 
+@Log4j2
 public final class Core extends PluginBase {
 
     @Override
@@ -26,7 +27,7 @@ public final class Core extends PluginBase {
             pluginManager.registerEvents(injector.getInstance(AuthenticationListener.class), this);
             pluginManager.registerEvents(injector.getInstance(FormListener.class), this);
         } catch (Throwable throwable) {
-            getServer().getLogger().logException(throwable);
+            log.fatal("An error occurred during startup", throwable);
             System.exit(1);
         }
     }
@@ -45,7 +46,6 @@ public final class Core extends PluginBase {
             bind(Core.class).toInstance(core);
             Server server = core.getServer();
             bind(Server.class).toInstance(server);
-            bind(MainLogger.class).toInstance(server.getLogger());
             bind(ServerScheduler.class).toInstance(server.getScheduler());
 
             //Hibernate
