94bf3c49979f88ff59c7305517bbb99d42679d9b
Bestaford <butenko1demitr@gmail.com>
2023-05-14 21:13:13 +0300

    Void generator

--- src/main/java/ru/bestaford/ariovale/Core.java
+++ src/main/java/ru/bestaford/ariovale/Core.java
@@ -1,6 +1,7 @@
 package ru.bestaford.ariovale;
 
 import cn.nukkit.Server;
+import cn.nukkit.level.generator.Generator;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.plugin.PluginManager;
 import cn.nukkit.scheduler.ServerScheduler;
@@ -14,6 +15,7 @@ import org.hibernate.cfg.Configuration;
 import ru.bestaford.ariovale.listener.AuthenticationListener;
 import ru.bestaford.ariovale.listener.FormListener;
 import ru.bestaford.ariovale.service.*;
+import ru.bestaford.ariovale.util.VoidGenerator;
 
 import java.util.Objects;
 
@@ -23,8 +25,8 @@ public final class Core extends PluginBase {
     @Override
     public void onEnable() {
         try {
+            Generator.addGenerator(VoidGenerator.class, "void", Generator.TYPE_INFINITE);
             Injector injector = Guice.createInjector(Stage.DEVELOPMENT, new Module(this));
-
             PluginManager pluginManager = getServer().getPluginManager();
             pluginManager.registerEvents(injector.getInstance(AuthenticationListener.class), this);
             pluginManager.registerEvents(injector.getInstance(FormListener.class), this);
---
+++ src/main/java/ru/bestaford/ariovale/util/VoidGenerator.java
@@ -0,0 +1,63 @@
+package ru.bestaford.ariovale.util;
+
+import cn.nukkit.level.ChunkManager;
+import cn.nukkit.level.generator.Generator;
+import cn.nukkit.math.NukkitRandom;
+import cn.nukkit.math.Vector3;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class VoidGenerator extends Generator {
+
+    private ChunkManager chunkManager;
+    private final Map<String, Object> options;
+
+    public VoidGenerator() {
+        this(Collections.emptyMap());
+    }
+
+    public VoidGenerator(Map<String, Object> options) {
+        this.options = options;
+    }
+
+    @Override
+    public int getId() {
+        return TYPE_INFINITE;
+    }
+
+    @Override
+    public ChunkManager getChunkManager() {
+        return chunkManager;
+    }
+
+    @Override
+    public void init(ChunkManager chunkManager, NukkitRandom random) {
+        this.chunkManager = chunkManager;
+    }
+
+    @Override
+    public void generateChunk(int chunkX, int chunkZ) {
+
+    }
+
+    @Override
+    public void populateChunk(int chunkX, int chunkZ) {
+
+    }
+
+    @Override
+    public Map<String, Object> getSettings() {
+        return options;
+    }
+
+    @Override
+    public String getName() {
+        return "void";
+    }
+
+    @Override
+    public Vector3 getSpawn() {
+        return new Vector3(0.5, 256, 0.5);
+    }
+}
